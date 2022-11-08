414f5d54c79f6621a3a91149831d4cc5b8616297
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-11-09 00:20:29 +0200

    8.2 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_TIME_DATA = 'videoplayer-current-time';
+const timeFromStorage = localStorage.getItem(STORAGE_TIME_DATA)
+
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+player.setCurrentTime(timeFromStorage);
+
+
+function saveCurrentTime(time) {
+    const currentTime = time.seconds;
+    localStorage.setItem(STORAGE_TIME_DATA, currentTime);
+}
+
 
